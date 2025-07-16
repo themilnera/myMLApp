068fc0f520cf6dcddc02f0cafcc968c8e71ce32f
themilnera <amilner32@gmail.com>
2025-07-16 12:17:14 -0600

    Created a basic machine learning model meant to judge sentiment in a given example text. Trained off of a collection of yelp review examples.

--- myMLApp/Program.cs
+++ myMLApp/Program.cs
@@ -10,4 +10,4 @@ var result = SentimentModel.Predict(sampleData);
 
 // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
 var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
-Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}"); 
